96c3f7b7632c9b1974469d43c7438e7b2c9d866b
Rutvij <Rudy@Rutvijs-MacBook-Pro.local>
2025-08-05 21:46:24 -0600

    Add Astro GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rrana-21.github.io',
+  base: process.env.NODE_ENV === 'production' ? '/web' : '',
   devToolbar: {
     enabled: false
   },
