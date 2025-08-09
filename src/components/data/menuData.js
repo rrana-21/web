e64946ffafaa6deca0cef5f50b640e3c91a8bb23
Rutvij <Rudy@Rutvijs-MacBook-Pro.local>
2025-08-08 21:07:56 -0600

    Update LinkedIn profile URL in navigation

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -3,7 +3,7 @@ export const mainMenu = [
      {link:"#about", content:"About", icon:"octicon:person-24"},
      {link:"#skills", content:"Skills", icon:"carbon:skill-level-advanced"},
      {link:"#portfolio", content:"Selected Work", icon:"carbon:portfolio"},
-     {link:"https://www.linkedin.com/in/yourprofile", content:"", icon:"mdi:linkedin", external: true, iconOnly: true},
+     {link:"https://www.linkedin.com/in/rutvijrana/", content:"", icon:"mdi:linkedin", external: true, iconOnly: true},
      {link:"https://github.com/rrana-21", content:"", icon:"mdi:github", external: true, iconOnly: true}
 ]
 
